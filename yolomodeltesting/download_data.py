f20af50593c87d4d7cf40fadceabcba22aff1313
ying.j <ying.j@intelab.cloud>
2021-03-22 15:01:20 +0800

    show confusion matrix

---
+++ yolomodeltesting/download_data.py
@@ -0,0 +1,30 @@
+import os
+import pandas as pd
+import ast
+import requests
+from tqdm import tqdm
+import cv2
+from autocollection.aliyun_oss import oss_delete_file
+df = pd.read_csv('ultraundetacted_03-22.txt')
+
+
+for index, row in tqdm(df.iterrows()):
+    did = row['device_id']
+    url = row["video_url"]
+    print(url)
+    vcap = cv2.VideoCapture(url)
+    success, frame = vcap.read()
+    cv2.imwrite(,frame)
+    # location = ast.literal_eval(location)
+        # print(row)
+        # print(location[0])
+
+# with open('2021-03-02-10-13-43_EXPORT_CSV_2176571_340_0.csv','r') as txt:
+#     lines = txt.readlines()
+#
+# for li in lines:
+#     li = li.replace('"','')
+#     elements = li.split(',')
+#     type = elements[1]
+#     location = elements[2]
+#     url = elements[3]
